062def7c3719fca240b836c053a5db6650c93f3c
Patrick Grey <websemantic@yahoo.co.uk>
2019-09-18 15:17:11 +0100

    Add backend content

--- front-end/src/routes/index.svelte
+++ front-end/src/routes/index.svelte
@@ -1,19 +1,55 @@
 <script context="module">
   export async function preload(page) {
-    return this.fetch(
+    const htmlPostsCount = 5;
+
+    const pages = this.fetch(
       `http://localhost:2368/ghost/api/v2/content/pages/?key=8f61b29cf34abca369566ed9a6`
-    )
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        console.log(data);
-      });
+    ).then(response => {
+      return response.json();
+    });
+
+    const htmlPosts = this.fetch(
+      `http://localhost:2368/ghost/api/v2/content/posts/?key=8f61b29cf34abca369566ed9a6&fields=id,title,url,html&limit=${htmlPostsCount}`
+    ).then(response => {
+      return response.json();
+    });
+
+    const posts = this.fetch(
+      `http://localhost:2368/ghost/api/v2/content/posts/?key=8f61b29cf34abca369566ed9a6&fields=id,title,url&limit=100`
+    ).then(response => {
+      return response.json();
+    });
+
+    function filterPage(pagesObject) {
+      const pagename = page.path != "/" ? page.path : "index";
+      return pagesObject.pages.filter(i => pagename.includes(i.slug));
+    }
+
+    let combinedData = { page: {}, htmlPosts: {}, posts: {} };
+    // let pagedata;
+
+    return Promise.all([pages, htmlPosts, posts]).then(function(values) {
+      combinedData["page"] = filterPage(values[0])[0];
+      combinedData["htmlPosts"] = values[1];
+      combinedData["posts"] = values[2];
+      // pagedata = combinedData["page"];
+      // console.log(combinedData["page"]);
+      // console.log(
+      //   combinedData["posts"].posts.splice(
+      //     htmlPostsCount,
+      //     combinedData["posts"].posts.length
+      //   )
+      // );
+      // console.log(combinedData["posts"].posts);
+      return combinedData;
+    });
   }
 </script>
 
 <script>
-
+  export let page;
+  export let htmlPosts;
+  export let posts;
 </script>
 
 <style>
@@ -31,20 +67,6 @@
     margin: 0 0 0.5em 0;
   }
 
-  figure {
-    margin: 0 0 1em 0;
-  }
-
-  img {
-    width: 100%;
-    max-width: 400px;
-    margin: 0 0 1em 0;
-  }
-
-  p {
-    margin: 1em auto;
-  }
-
   @media (min-width: 480px) {
     h1 {
       font-size: 4em;
@@ -53,18 +75,11 @@
 </style>
 
 <svelte:head>
-  <title>Sapper project template</title>
+  <title>Hi</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>{page.title}</h1>
 
-<figure>
-  <img alt="Borat" src="great-success.png" />
-  <figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p>
-  <strong>
-    Try editing this file (src/routes/index.svelte) to test live reloading.
-  </strong>
-</p>
+<div class="pg-container">
+  {@html page.html}
+</div>
