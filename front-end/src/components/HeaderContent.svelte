43aef3e8dd246c9f198f61eccb66fcd8b24391de
Patrick Grey <websemantic@yahoo.co.uk>
2019-09-25 12:44:39 +0100

    Formatting dates of publish

--- front-end/src/components/HeaderContent.svelte
+++ front-end/src/components/HeaderContent.svelte
@@ -11,7 +11,7 @@
     transform-origin: bottom right;
     transform: rotate(-90deg);
     color: #999;
-    background: rgb(153, 153, 153);
+    /* background: rgb(153, 153, 153); */
     background: linear-gradient(
       179deg,
       rgba(153, 153, 153, 0) 0%,
@@ -21,7 +21,7 @@
       rgba(153, 153, 153, 0) 57%,
       rgba(153, 153, 153, 0) 100%
     );
-    background-position: bottom 0.1vh right -75.5vh;
+    background-position: bottom 0.1vh right -85.5vh;
     background-repeat: no-repeat;
     transition: font-size 0.3s, background-position 0.3s;
   }
@@ -29,17 +29,9 @@
   @media (min-width: 768px) {
     h1 {
       font-size: 7vh;
-      background-position: bottom -0.3vh right -57vh;
+      background-position: bottom -0.3vh right -55vh;
     }
   }
 </style>
 
-<!-- <h1>The Grey line</h1> -->
-<h1>Patrick Grey</h1>
-<!-- <button>About</button>
-      <div class="pg-about">
-        <p>
-          I'm Patrick Grey. I develop websites, applications and motion graphic
-          videos.
-        </p>
-      </div> -->
+<h1>The Grey Line</h1>
--- front-end/src/routes/index.svelte
+++ front-end/src/routes/index.svelte
@@ -9,13 +9,13 @@
     });
 
     const htmlPosts = this.fetch(
-      `http://localhost:2368/ghost/api/v2/content/posts/?key=8f61b29cf34abca369566ed9a6&fields=id,title,url,excerpt&formats=plaintext&limit=${htmlPostsCount}`
+      `http://localhost:2368/ghost/api/v2/content/posts/?key=8f61b29cf34abca369566ed9a6&fields=id,title,published_at,updated_at,url,excerpt&formats=plaintext&limit=${htmlPostsCount}`
     ).then(response => {
       return response.json();
     });
 
     const posts = this.fetch(
-      `http://localhost:2368/ghost/api/v2/content/posts/?key=8f61b29cf34abca369566ed9a6&fields=id,title,url&limit=100`
+      `http://localhost:2368/ghost/api/v2/content/posts/?key=8f61b29cf34abca369566ed9a6&fields=id,title,published_at,updated_at,url&limit=100`
     ).then(response => {
       return response.json();
     });
@@ -43,11 +43,75 @@
   export let postsData;
   export let htmlPostsCount;
   const noHtmlPosts = postsData.posts.slice(htmlPostsCount);
+
+  // Process dates
+  // console.log(htmlPostsData.posts.length);
+
+  function formatTime(dateString) {
+    return new Date(dateString).toLocaleTimeString("en-GB");
+  }
+
+  function formatDate(dateString) {
+    return new Date(dateString).toDateString();
+  }
+
+  // formatDates();
 </script>
 
 <style>
-  h1 {
-    margin-top: 0;
+  h2 {
+    color: #666666;
+    display: inline-block;
+    /* box-shadow: inset 0 -15px 0 10px #eee; */
+    background: linear-gradient(
+      179deg,
+      rgba(153, 153, 153, 0) 0%,
+      rgba(153, 153, 153, 0) 42%,
+      rgba(153, 153, 153, 0.2) 43%,
+      rgba(153, 153, 153, 0.2) 57%,
+      rgba(153, 153, 153, 0) 58%,
+      rgba(153, 153, 153, 0) 100%
+    );
+    background-position: bottom -0.1em right 0;
+    background-repeat: no-repeat;
+    margin-bottom: 0.5em;
+    line-height: 0.9em;
+  }
+
+  .pg-page-container {
+    background: #eee;
+    /* border: 1px solid #eee; */
+    padding: 1em;
+    border-radius: 5px;
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.12), 0 2px 2px rgba(0, 0, 0, 0.12),
+      0 4px 4px rgba(0, 0, 0, 0.12), 0 8px 8px rgba(0, 0, 0, 0.12),
+      0 16px 16px rgba(0, 0, 0, 0.12);
+  }
+
+  .pg-posts {
+    list-style: none;
+    margin: 0;
+    margin-top: 1.5em;
+    padding: 0;
+  }
+
+  .pg-dates {
+    font-size: 0.7em;
+    color: #999;
+  }
+
+  .pg-post-excerpt-long::after {
+    content: "";
+    display: inline-block;
+    margin-left: -60px;
+    width: 60px;
+    height: 1em;
+    background: rgb(255, 255, 255);
+    background: linear-gradient(
+      90deg,
+      rgba(255, 255, 255, 0) 0%,
+      rgba(255, 255, 255, 1) 80%
+    );
   }
 </style>
 
@@ -55,31 +119,46 @@
   <title>{page.title}</title>
   <meta
     name="Description"
-    content="Patrick Grey: web and video developer, international man of
-    mystery!" />
+    content="The Grey Line: web and video development studio." />
 </svelte:head>
 
-<h1>{page.title}</h1>
+<!-- <h1>{page.title}</h1> -->
 
-<div class="pg-container">
+<div class="pg-page-container">
   {@html page.html}
 </div>
 
-<ul>
+<ul class="pg-posts">
 
   {#each htmlPostsData.posts as htmlpost}
     <li>
-      <p>{htmlpost.title}</p>
-      <p>
-        {@html htmlpost.excerpt}
-        ...
+      <h2>{htmlpost.title}</h2>
+      <div class="pg-dates">
+        {#if new Date(htmlpost.updated_at) > new Date(htmlpost.published_at)}
+          <p>
+            Published: 📅 {formatDate(htmlpost.published_at)} ⌚ {formatTime(htmlpost.published_at)}
+          </p>
+          <p>
+            (Updated: {formatDate(htmlpost.updated_at)} {formatTime(htmlpost.updated_at)})
+          </p>
+        {:else}
+          <p>
+            Published: 📅 {formatDate(htmlpost.published_at)} ⌚ {formatTime(htmlpost.published_at)}
+          </p>
+        {/if}
+      </div>
+      <p
+        class="pg-post-excerpt {htmlpost.excerpt.length > 200 ? 'pg-post-excerpt-long' : ''}">
+        {@html htmlpost.excerpt.substring(0, 200)}
       </p>
     </li>
   {/each}
 
   {#each noHtmlPosts as post}
     <li>
-      <p>{post.title}</p>
+      <h2>{post.title}</h2>
+      <p>{new Date(post.published_at)}</p>
+      <p>{new Date(post.updated_at)}</p>
     </li>
   {/each}
 </ul>
