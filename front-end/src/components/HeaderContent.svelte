1d767c04d6ef99a5d78cd363cdedc5dfd0abf2e3
Patrick Grey <websemantic@yahoo.co.uk>
2019-10-02 09:54:55 +0100

    Fixed background on h1

--- front-end/src/components/HeaderContent.svelte
+++ front-end/src/components/HeaderContent.svelte
@@ -4,30 +4,15 @@
     display: inline-block;
     padding: 0;
     margin: 0;
-
     font-size: 2em;
     font-family: "Lora", Georgia, "Times New Roman", Times, serif;
-    /* transform-origin: bottom right;
-    transform: rotate(-90deg); */
     color: #999;
-    /* background: rgb(153, 153, 153); */
-    background: linear-gradient(
-      179deg,
-      rgba(153, 153, 153, 0) 0%,
-      rgba(153, 153, 153, 0) 43%,
-      rgba(153, 153, 153, 0.35) 45%,
-      rgba(153, 153, 153, 0.35) 55%,
-      rgba(153, 153, 153, 0) 57%,
-      rgba(153, 153, 153, 0) 100%
-    );
-    background-position: top 0.1em left;
-    background-repeat: no-repeat;
-    /* transition: font-size 0.3s, background-position 0.3s; */
   }
 
   @media (min-width: 600px) {
     h1 {
       width: 100vh;
+
       position: absolute;
       right: 0.25em;
       top: -0.4em;
@@ -37,7 +22,6 @@
       font-size: 2.4em;
       transform-origin: bottom right;
       transform: rotate(-90deg);
-      background-position: bottom 0.05em right -18.2em;
     }
   }
 
@@ -45,7 +29,44 @@
     h1 {
       font-size: 3.8em;
       top: -0.75em;
-      background-position: bottom 0em right -9.2em;
+    }
+  }
+
+  @supports (mix-blend-mode: lighten) {
+    h1 {
+      background: #000;
+      color: white;
+      mix-blend-mode: lighten;
+    }
+
+    @media (min-width: 600px) {
+      h1 {
+        width: max-content;
+      }
+    }
+
+    h1::before {
+      content: "";
+      display: inline-block;
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      pointer-events: none;
+      background: linear-gradient(
+        90deg,
+        rgba(153, 153, 153, 1) 0%,
+        rgba(153, 153, 153, 1) 60%,
+        rgba(239, 250, 0, 1) 65%,
+        rgba(255, 153, 0, 1) 75%,
+        rgba(255, 80, 60, 1) 80%,
+        rgba(255, 151, 252, 1) 85%,
+        rgba(112, 220, 255, 1) 90%,
+        rgba(51, 190, 0, 1) 95%
+      );
+      mix-blend-mode: multiply;
+      border: 1px solid black;
     }
   }
 </style>
