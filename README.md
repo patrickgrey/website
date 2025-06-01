14f90138bd5fcdf48cc98177a9cc46e537a0e397
patrickgrey <websemantic@yahoo.co.uk>
2025-06-01 13:53:51 +0100

    Added reminder to run decap server

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Patrick Grey's website
+Remember to run `npm run decap` before the dev server when developing locally you dufus! (Yes, I lost a couple of hours when I forgot this)
--- functions/api/auth.js
+++ functions/api/auth.js
@@ -20,6 +20,7 @@ export async function onRequest(context) {
             'state',
             crypto.getRandomValues(new Uint8Array(12)).join(''),
         );
+
         return Response.redirect(redirectUrl.href, 301);
 
     } catch (error) {
