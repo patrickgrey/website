a40f656f2e6dc6b49de535b513b32905a93c8fd5
patrickgrey <websemantic@yahoo.co.uk>
2025-06-09 17:19:19 +0100

    Profile disguises activated

---
+++ website-source/_shared/js/shared.js
@@ -0,0 +1,54 @@
+(() => {
+    const container = document.querySelector(`header .pg-profile-container>div.pg-profile-container-inner`);
+    const profileTop = container.querySelector(`div.pg-profile-image-top`);
+    const profileBottom = container.querySelector(`div.pg-profile-image-bottom`);
+    const profileSVG = container.querySelector(`svg`);
+
+    const specs1 = profileSVG.querySelector(`#specs-1`);
+    const tash1 = profileSVG.querySelector(`#tash-1`);
+    const beard1 = profileSVG.querySelector(`#beard-1`);
+    const hat1 = profileSVG.querySelector(`#hat-1`);
+
+
+    function pickDisguise() {
+        const partsArray = [specs1, tash1, beard1, hat1]
+        let chosenCount = 0
+        partsArray.forEach(item => {
+            item.style.display = "none"
+            if (Math.round(Math.random()) === 1) {
+                chosenCount++
+                item.style.display = "block"
+            }
+        })
+
+        if (chosenCount === 0) {
+            let random = Math.floor(Math.random() * partsArray.length);
+            let item = partsArray[random];
+            item.style.display = "block"
+        }
+    }
+
+    function initProfile() {
+        profileTop?.addEventListener("transitionend", (event) => {
+            // console.log("event: ", event.target);
+            const top = event.target
+            // console.log("profileTop: ", top.getBoundingClientRect());
+            // console.log("profileBottom: ", profileBottom.getBoundingClientRect());
+
+            if (
+                profileTop.getBoundingClientRect().x <= (profileBottom.getBoundingClientRect().x + 10)
+                &&
+                profileTop.getBoundingClientRect().x >= (profileBottom.getBoundingClientRect().x - 10)
+            ) {
+                pickDisguise()
+            }
+
+        })
+    }
+
+    function init() {
+        initProfile()
+    }
+
+    init()
+})();
